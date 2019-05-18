668551c1955c5355aab3f3afc97a81e4d0bd984a
Peter J. Mello <admin@petermello.net>
2019-05-18 13:30:05 -0700

    Correct CircleCI artifact collection paths
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- libgnome2-vte-perl/make_debian.sh
+++ libgnome2-vte-perl/make_debian.sh
@@ -1,25 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Some working variables
-G="\033[32m"
-B="\033[39m"
-Y="\033[33m"
-OK="${G}OK${B}"
-ERROR="${Y}ERROR${B}"
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+# Some ANSI color goodness
+G='\033[32m'
+B='\033[39m'
+Y='\033[33m'
+OK="${G}OK:${B}"
+ERROR="${Y}ERROR:${B}"
 
 PACKAGE_DIR=./tmp
 
 rm -rf $PACKAGE_DIR
 mkdir $PACKAGE_DIR
 
-wget -q http://search.cpan.org/CPAN/authors/id/X/XA/XAOC/Gnome2-Vte-0.11.tar.gz -O $PACKAGE_DIR/Gnome2-Vte-0.11.tar.gz 
+wget -q http://search.cpan.org/CPAN/authors/id/X/XA/XAOC/Gnome2-Vte-0.11.tar.gz -O $PACKAGE_DIR/libgnome2-vte-perl_0.11.orig.tar.gz || echo "An error occured while downloading Gnome2-Vte"
 
-if [ $? -ne 0 ]; then
-        echo "An error occured while downloading Gnome2-Vte"
-        exit 1
+if [ ! -f "$PACKAGE_DIR/libgnome2-vte-perl_0.11.orig.tar.gz" ]; then
+  exit 1
 fi
 
-tar -xzf tmp/Gnome2-Vte-0.11.tar.gz -C $PACKAGE_DIR
+tar -xzf $PACKAGE_DIR/libgnome2-vte-perl_0.11.orig.tar.gz -C $PACKAGE_DIR
 
 cp -R debian/ $PACKAGE_DIR/Gnome2-Vte-0.11/debian/
 
@@ -27,16 +35,6 @@ cd $PACKAGE_DIR/Gnome2-Vte-0.11/
 
 perl -i -pe "s/unstable/$(lsb_release -cs)/" debian/changelog
 
-mv ../Gnome2-Vte-0.11.tar.gz ../libgnome2-vte-perl_0.11.orig.tar.gz
-
 echo -n "Building package release, be patient ..."
 
-debuild -F -us -uc
-
-if [ $? -eq 0 ] ; then
-  echo -e " $OK !"
-  echo "This look like good news, package succesfully build in $PACKAGE_DIR :)"
-else
-  echo -e "${ERROR}"
-  echo "Bad news, something did not happen as expected, check .build file in $PACKAGE_DIR to get more information."
-fi
+debuild -F -us -uc && echo "$OK I have good news, the package was succesfully built in $(readlink -m "$PACKAGE_DIR") :)" || echo "$ERROR I have bad news; the build process was unable to complete successfully, please check the .build file in $(readlink -m "$PACKAGE_DIR") to get more information."
