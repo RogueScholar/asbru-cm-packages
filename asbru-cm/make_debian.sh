4f7564cb5ec1d64ffd4a734904fcb5e8005f4051
Peter J. Mello <admin@petermello.net>
2019-05-20 13:01:07 -0700

    Rewrite Asbru make_debian script to follow libraries style

--- asbru-cm/make_debian.sh
+++ asbru-cm/make_debian.sh
@@ -9,93 +9,58 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-# Some ANSI color goodness
-G='\033[32m'
-B='\033[39m'
-Y='\033[33m'
+# Some ANSI escape color goodness
+G='\033[32;40m'
+B='\033[39;49m'
+Y='\033[33;40m'
 OK="${G}OK:${B}"
 ERROR="${Y}ERROR:${B}"
 
+# Information about the git repository and build directory saved to variables
+REPO_ROOT_DIR="$(git rev-parse --show-toplevel)"
+PACKAGE_DIR="${REPO_ROOT_DIR}/asbru-cm/tmp"
+PACKAGE_NAME="asbru-cm"
+PACKAGE_VER="$(curl -s https://api.github.com/repos/asbru-cm/asbru-cm/releases/latest | grep "tag_name" | awk '{print $2}' | tr -d , | tr -d '"')"
+DOWNLOAD_URL="$(curl -s https://api.github.com/repos/asbru-cm/asbru-cm/releases/latest | grep "tarball_url" | cut -d : -f 2,3 | tr -d , | tr -d '"')"
+PACKAGE_ARCH="$(dpkg --print-architecture)"
+
 # Let's check some basic requirements
-if ! [ -x "$(command -v debuild)" ]; then
-  echo "debuild is required, did you install the 'devscripts' package yet ?"
+if [ ! -x "$(command -v debuild)" ]; then
+  echo "debuild is required, did you install the 'devscripts' package yet?"
   exit 1
 fi
 
-# Some other optional requirements
-if [ -x "$(command -v jq)" ]; then
-  JQ=$(command -v jq)
-else
-  JQ=""
-fi
-if [ -x "$(command -v curl)" ]; then
-  CURL=$(command -v curl)
-else
-  CURL=""
-fi
-
-if [[ -z "$1" ]]; then
-  if [ ! "$JQ" == "" ] && [ ! "$CURL" == "" ] ; then
-    # Try to guess the latest release
-    echo -n "No release given, querying GitHub ..."
-    all_releases=$(${CURL} -s https://api.github.com/repos/asbru-cm/asbru-cm/tags)
-    if [ $? -eq 0 ] ; then
-      echo -e " $OK !"
-      echo -n -e "Extracting latest version ..."
-      RELEASE=$(echo $all_releases | ${JQ} -r '.[0] | .name')
-      echo -e " found [$RELEASE], $OK !"
-    fi
-  fi
-else
-  RELEASE=$1
-fi
-
-if [[ -z "$RELEASE" ]] ; then
-  echo -e "${ERROR}"
-  echo "Either we could not fetch the latest release or no releasename is given." 1>&2
-  echo "Please provide a release name matching GitHub. It is case sensitive like 5.0.0-RC1" 1>&2
-  echo "You can find Ásbrú releases at https://github.com/asbru-cm/asbru-cm/releases" 1>&2
-  echo " " 1>&2
-  echo "If you want this tool to guess the latest release automatically, make sure the following tools are available:" 1>&2
-  echo " - curl (https://curl.haxx.se/)" 1>&2
-  echo " - jq (https://stedolan.github.io/jq)" 1>&2
-  echo " " 1>&2
+if [ ! -x "$(command -v curl)" ]; then
+  echo "curl is required, please install it with 'sudo apt install curl' and try again."
   exit 1
 fi
 
-PACKAGE_DIR=./tmp
-RELEASE_DEBIAN=${RELEASE,,}
-DEBIAN_VERSION=${RELEASE_DEBIAN/-/"~"}
+# Delete the build directory if it exists from earlier attempts and create it anew and empty
+rm -rf "${PACKAGE_DIR}"
+mkdir -p "${PACKAGE_DIR}"
 
-echo -n "Building package release ${DEBIAN_VERSION}, be patient ..."
+# Download the tarball of the latest tagged release from GitHub
+wget -qc -t 5 --show-progress "${DOWNLOAD_URL}" -O "${PACKAGE_DIR}"/${PACKAGE_NAME}_"${PACKAGE_VER}".orig.tar.gz
 
-rm -rf $PACKAGE_DIR
-mkdir $PACKAGE_DIR
-
-wget -q https://github.com/asbru-cm/asbru-cm/archive/$RELEASE.tar.gz -O $PACKAGE_DIR/asbru-cm_$RELEASE_DEBIAN.orig.tar.gz
-
-if [ $? -ne 0 ]; then
-  echo "An error occured while downloading release $RELEASE"
-  echo "Please check if that release actually exists and the server isn't down"
+# Error out of the script if no file was able to be downloaded
+if [ ! -f "${PACKAGE_DIR}/${PACKAGE_NAME}_${PACKAGE_VER}.orig.tar.gz" ]; then
   exit 1
 fi
 
-tar -xzf $PACKAGE_DIR/asbru-cm_$RELEASE_DEBIAN.orig.tar.gz -C $PACKAGE_DIR
+# Unpack the module in a directory equivalent to its CPAN name with version
+tar -xzf "${PACKAGE_DIR}"/${PACKAGE_NAME}_"${PACKAGE_VER}".orig.tar.gz -C "${PACKAGE_DIR}"
 
-cp -R debian/ $PACKAGE_DIR/asbru-cm-$RELEASE/debian/
+# Copy our Debian packaging files into the same directory as the source code
+cp -R debian/ "${PACKAGE_DIR}"/${PACKAGE_NAME}-"${PACKAGE_VER}"/
 
-cd $PACKAGE_DIR/asbru-cm-$RELEASE/
+# Make that source+packaging directory the new working directory
+cd "${PACKAGE_DIR}"/${PACKAGE_NAME}-"${PACKAGE_VER}"
 
-mv ../asbru-cm_$RELEASE_DEBIAN.orig.tar.gz ../asbru-cm_$DEBIAN_VERSION.orig.tar.gz
+# Replace the generic distribution string "unstable" with the distribution code-name of the build system
+perl -i -pe "s/unstable/$(lsb_release -cs)/" debian/changelog
 
-debuild -us -uc > /dev/null
+# Warn user of potentially lengthy process ahead
+echo -n "Building package ${PACKAGE_NAME}_${PACKAGE_VER}_${PACKAGE_ARCH}.deb, please be patient..."
 
-if [ $? -eq 0 ] ; then
-  echo -e " $OK !"
-  echo "This look like good news, package succesfully build in $PACKAGE_DIR :)"
-else
-  echo -e "${ERROR}"
-  echo "Bad news, something did not happen as expected, check .build file in $PACKAGE_DIR to get more information."
-fi
-
-echo "All done."
+# Call debuild to oversee the build process and produce an output string for the user based on its exit code
+debuild -F -us -uc && echo "${OK} I have good news! ${PACKAGE_NAME}_${PACKAGE_VER}_${PACKAGE_ARCH}.deb was successfully built in ${PACKAGE_DIR} :)" || echo "${ERROR} I have bad news; the build process was unable to complete successfully. Please check the ${PACKAGE_NAME}_${PACKAGE_VER}_${PACKAGE_ARCH}.build file in ${PACKAGE_DIR} to get more information."
